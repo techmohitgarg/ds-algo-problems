db1a5cd9750e2ba1b518e3ad0a6a9256cf055081
Mohit garg <mohit.garg@bookmyshow.com>
2019-05-30 19:23:34 +0530

    some more problems solutions

--- src/com/mohit/code_chef_problems/PERMUT2_AmbiguousPermutations.java
+++ src/com/mohit/code_chef_problems/PERMUT2_AmbiguousPermutations.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class PERMUT2_AmbiguousPermutations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t > 0) {
-            int n = sc.nextInt();
+        int n = sc.nextInt();
+        while (n != 0) {
             int[] num = new int[n];
             for (int i = 0; i < n; i++) {
                 num[i] = sc.nextInt();
             }
             // Set result
             System.out.println((isAmbigouesPermutations(num)));
-            t--;
+            n = sc.nextInt();
         }
     }
 
