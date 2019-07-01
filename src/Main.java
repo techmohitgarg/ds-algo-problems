022e75b84a339805330d1e1dbd3fa2ccad0e06f8
Mohit garg <mohit.garg@bookmyshow.com>
2019-07-01 19:03:24 +0530

    some more problems solutions

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,46 @@
 import java.math.BigInteger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
-Example 1:
-
-Input: version1 = "0.1", version2 = "1.1"
-Output: -1
-Example 2:
-
-Input: version1 = "1.0.1", version2 = "1"
+Input: ["23:59","00:00"]
 Output: 1
-Example 3:
-
-Input: version1 = "7.5.2.4", version2 = "7.5.3"
-Output: -1
-Example 4:
-
-Input: version1 = "1.01", version2 = "1.001"
-Output: 0
-Explanation: Ignoring leading zeroes, both “01” and “001" represent the same number “1”
-Example 5:
-
-Input: version1 = "1.0", version2 = "1.0.0"
-Output: 0
-Explanation: The first version number does not have a third level revision number, which means its third level revision number is default to "0"
  */
 public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        System.out.println(main.compareVersion("111111111111111111112.0", "111111111111111111112.1"));
+        List<String> list = new ArrayList<>();
+        list.add("23:59");
+        list.add("11:05");
+        System.out.println(main.findMinDifference(list));
     }
 
-    public int compareVersion(String version1, String version2) {
-        if (version1.length() == 0 && version2.length() == 0) return 0;
-        if (version1.length() == 0) return -1;
-        if (version2.length() == 0) return 1;
-        int i = 0;
-        int j = 0;
-        while (i < version1.length() || j < version2.length()) {
-            while (i < version1.length() && version1.charAt(i) == '0') i++;
-            while (j < version2.length() && version2.charAt(j) == '0') j++;
-            while (i < version1.length() && version1.charAt(i) != '.' || j < version2.length() && version2.charAt(j) != '.') {
-                int b1 = 0;
-                if (i < version1.length()) {
-                    b1 = version1.charAt(i) - '0';
-                }
+    public int findMinDifference(List<String> timePoints) {
+        int mindiff = Integer.MAX_VALUE;
+        int n = timePoints.size();
+        Collections.sort(timePoints);
+        for (int i = 0; i < timePoints.size(); i++) {
+            int diff = Math.abs(timeDiff(timePoints.get((i - 1 + n) % n), timePoints.get(i)));
+            diff = Math.min(diff, 1440 - diff);
+            mindiff = Math.min(mindiff, diff);
+        }
+        return mindiff;
+    }
 
-                int b2 = 0;
-                if (j < version2.length()) {
-                    b2 = version2.charAt(j) - '0';
-                }
-                if (b1 > b2) {
-                    return 1;
-                }
-                if (b1 < b2) {
-                    return -1;
-                }
-                i++;
-                j++;
-            }
-
-            i++;
-            j++;
+    private int timeDiff(String s1, String s2) {
+        try {
+            Date dt1 = new SimpleDateFormat("HH:mm").parse(s1);
+            Date dt2 = new SimpleDateFormat("HH:mm").parse(s2);
+            long diff = Math.abs(dt2.getTime() - dt1.getTime());
+            long diffMinutes = (diff / 1000) / 60;
+            return (int) diffMinutes;
+        } catch (ParseException e) {
         }
         return 0;
     }
-
 }
 
