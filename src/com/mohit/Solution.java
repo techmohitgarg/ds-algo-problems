09e314280925f34e3a8a712017a7c5c31e0f6367
Mohit garg <mohit.garg@bookmyshow.com>
2019-07-08 19:21:46 +0530

    some more problems solutions

--- src/com/mohit/Solution.java
+++ src/com/mohit/Solution.java
@@ -3,6 +3,10 @@ package com.mohit;
 import com.mohit.linklist.ListNode;
 import com.mohit.linklist.PrintLinkList;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 public class Solution {
 
     public static void main(String[] args) {
@@ -10,40 +14,53 @@ public class Solution {
         ListNode node = new ListNode(1);
         node.next = new ListNode(2);
         node.next.next = new ListNode(3);
-        node.next.next.next = new ListNode(4);
-        node.next.next.next.next = new ListNode(5);
-        PrintLinkList.print(main.rotateRight(node, 2));
+        node.next.next.next = new ListNode(3);
+        node.next.next.next.next = new ListNode(4);
+        PrintLinkList.print(main.deleteDuplicates(node));
     }
 
-    public ListNode rotateRight(ListNode head, int k) {
-        if (head == null) {
-            return head;
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return head;
+
+        ListNode prev = new ListNode(-1);
+        prev.next = head;
+
+        ListNode curr = head;
+        while (curr != null) {
+            while (curr != null && prev.val == curr.next.val) {
+                curr = curr.next;
+            }
+            if (prev.val != curr.val) {
+                prev = prev.next;
+            } else {
+                prev = curr;
+            }
+            curr = prev;
         }
-        int len = 0;
+        return head;
+    }
+
+    public ListNode deleteDuplicates1(ListNode head) {
+        HashMap<Integer, Integer> set = new HashMap<>();
         ListNode iter = head;
+
         while (iter != null) {
-            len++;
+            set.put(iter.val, set.getOrDefault(iter.val, 0) + 1);
+            iter = iter.next;
+        }
+        ListNode node = new ListNode(0);
+        ListNode listNode = node;
+
+
+        iter = head;
+        while (iter != null) {
+            if (set.get(iter.val) == 1) {
+                listNode.next = new ListNode(iter.val);
+                listNode = listNode.next;
+            }
             iter = iter.next;
         }
 
-        int rotate = k % len;
-        if (rotate == 0) {
-            return head;
-        }
-        iter = head;
-        int i = 0;
-        while (i < (len - rotate) - 1) {
-            iter = iter.next;
-            i++;
-        }
-        ListNode node = iter.next;
-        iter.next = null;
-        ListNode listNode = node;
-        while (listNode.next != null) {
-            listNode = listNode.next;
-        }
-        listNode.next = head;
-        head = node;
-        return head;
+        return node.next;
     }
 }
