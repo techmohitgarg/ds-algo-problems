472fcda6be3116a620625cbc334e79d11a77e9b7
Mohit Garg <mohit.garg@bookmyshow.com>
2019-11-11 17:34:09 +0530

    Updated Code

--- src/com/mohit/TreeBookPractice.java
+++ src/com/mohit/TreeBookPractice.java
@@ -3,14 +3,52 @@ package com.mohit;
 import com.mohit.leetcode.tree.MakeTree;
 import com.mohit.tree.book_practice.binary_tree.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class TreeBookPractice {
 
 
     public static void main(String[] args) {
+       //[4,1,null,2,null,3]
+        System.out.println(new TreeBookPractice().rob(MakeTree.stringToTreeNode("[4,1,null,2,null,3]")));
+
     }
+
+
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.add(root);
+        nodes.add(null);
+        int sumOfParents = 0;
+        int sumOfChild = 0;
+        boolean isParent = true;
+        while (!nodes.isEmpty()) {
+            TreeNode node = nodes.poll();
+            if (node != null) {
+                if (isParent) {
+                    sumOfParents += node.val;
+                } else {
+                    sumOfChild += node.val;
+                }
+                if (node.left != null) nodes.add(node.left);
+                if (node.right != null) nodes.add(node.right);
+
+            } else {
+                isParent = !isParent;
+                if (!nodes.isEmpty()) {
+                    nodes.add(null);
+                }
+
+            }
+
+        }
+
+        return Math.max(sumOfChild, sumOfParents);
+    }
+
 
 }
--- src/com/mohit/leetcode/tree/medium/LowestCommonAncestorBinaryTree.java
+++ src/com/mohit/leetcode/tree/medium/LowestCommonAncestorBinaryTree.java
@@ -1,11 +1,20 @@
 package com.mohit.leetcode.tree.medium;
 
+import com.mohit.TreeBookPractice;
+import com.mohit.leetcode.tree.MakeTree;
 import com.mohit.tree.book_practice.binary_tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LowestCommonAncestorBinaryTree {
 
     public static void main(String[] s) {
-
+        TreeNode a = new TreeNode(5);
+        TreeNode b = new TreeNode(4);
+        //TreeNode reslt1 = new LowestCommonAncestorBinaryTree().lowestCommonAncestor(MakeTree.stringToTreeNode("[3,5,1,6,2,0,8,null,null,7,4]"), a, b);
+        TreeNode reslt1 = new LowestCommonAncestorBinaryTree().lowestCommonAncestor(MakeTree.stringToTreeNode("[3,5,1,6,2,0,8,null,null,7,4]"), a, b);
+        System.out.println(reslt1 == null ? "null" : reslt1.val);
 
     }
 
@@ -26,4 +35,40 @@ public class LowestCommonAncestorBinaryTree {
             return (left != null ? left : right);
         }
     }
+
+    /*public TreeNode lowestCommonAncestor(TreeNode root, TreeNode a, TreeNode b) {
+        if (root == null) {
+            return null;
+        }
+
+        List<TreeNode> path1 = new ArrayList<>();
+        List<TreeNode> path2 = new ArrayList<>();
+        if (!printAllAncestors(path1, root, a) || !printAllAncestors(path2, root, b)) {
+            return null;
+        }
+        int i;
+        for (i = 0; i < path1.size() && i < path2.size(); i++) {
+            if (!path1.get(i).equals(path2.get(i)))
+                break;
+        }
+        return path1.get(i - 1);
+    }
+
+    private boolean printAllAncestors(List<TreeNode> list, TreeNode node, TreeNode val) {
+        if (node == null) return false;
+
+
+        list.add(node);
+        if (node.val == val.val) return true;
+
+        if (printAllAncestors(list, node.left, val) ||
+                printAllAncestors(list, node.right, val)) return true;
+
+
+        list.remove(list.size() - 1);
+        return false;
+
+    }*/
+
+
 }
