a1e87875e8e657821593160a8dd36f09d4b60bd0
Mohit Garg <tech.mohit.garg@gmail.com>
2020-12-02 16:44:19 +0530

    Added Some test filkes

--- test/com/mohit/leetcode/array/easy/CheckArrayFormationThroughConcatenationTest.java
+++ test/com/mohit/leetcode/array/easy/CheckArrayFormationThroughConcatenationTest.java
@@ -1,2 +1,40 @@
-package com.mohit.leetcode.array.easy;public class CheckArrayFormationThroughConcatenationTest {
+package com.mohit.leetcode.array.easy;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckArrayFormationThroughConcatenationTest {
+
+    private CheckArrayFormationThroughConcatenation object;
+
+    @Before
+    public void setUp() throws Exception {
+        object = new CheckArrayFormationThroughConcatenation();
+    }
+
+    @Test
+    public void canFormArray1() {
+        Assert.assertEquals(true, object.canFormArray(new int[]{85}, new int[][]{{85}}));
+    }
+
+    @Test
+    public void canFormArray2() {
+        Assert.assertEquals(true, object.canFormArray(new int[]{15, 88}, new int[][]{{88}, {15}}));
+    }
+
+    @Test
+    public void canFormArray3() {
+        Assert.assertEquals(false, object.canFormArray(new int[]{49, 18, 16}, new int[][]{{16, 18, 49}}));
+    }
+
+    @Test
+    public void canFormArray4() {
+        Assert.assertEquals(true, object.canFormArray(new int[]{91, 4, 64, 78}, new int[][]{{78}, {4, 64}, {91}}));
+    }
+
+    @Test
+    public void canFormArray5() {
+        Assert.assertEquals(false, object.canFormArray(new int[]{1, 3, 5, 7}, new int[][]{{2, 4, 6, 8}}));
+    }
 }
--- test/com/mohit/leetcode/array/easy/CountNegativeNumbersInASortedMatrixTest.java
+++ test/com/mohit/leetcode/array/easy/CountNegativeNumbersInASortedMatrixTest.java
@@ -1,2 +1,33 @@
-package com.mohit.leetcode.array.easy;public class CountNegativeNumbersInASortedMatrixTest {
+package com.mohit.leetcode.array.easy;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CountNegativeNumbersInASortedMatrixTest {
+
+    CountNegativeNumbersInASortedMatrix object = null;
+
+    @Before
+    public void setUp() throws Exception {
+        object = new CountNegativeNumbersInASortedMatrix();
+    }
+
+    @Test
+    public void countNegatives1() {
+        Assert.assertEquals(8, object.countNegatives(new int[][]{{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}}));
+    }
+    @Test
+    public void countNegatives2() {
+        Assert.assertEquals(0, object.countNegatives(new int[][]{{3,2}, {1,0}}));
+    }
+    @Test
+    public void countNegatives3() {
+        Assert.assertEquals(3, object.countNegatives(new int[][]{{1,-1}, {-1,-1}}));
+    }
+    @Test
+    public void countNegatives4() {
+        Assert.assertEquals(1, object.countNegatives(new int[][]{{-1}}));
+    }
 }
